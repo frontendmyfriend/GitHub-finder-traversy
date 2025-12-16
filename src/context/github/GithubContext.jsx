18c532d8e9d5db86d2dda350229ca8c49dcf8803
Mary Ti <4younguniverse@gmail.com>
2025-12-16 16:30:52 +0200

    added actions file, refactored Context and User files

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,69 +1,23 @@
 import { createContext, useReducer } from "react";
 import githubReducer from "./GithubReducer";
-import loading from "daisyui/components/loading";
 
 const GithubContext = createContext();
-
-const GITHUB_URL = import.meta.env.VITE_GITHUB_URL;
-const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-//get search results
-const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q:text
-    })
-
-    const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    const {items} = await res.json();
-
-    dispatch({
-      type: "GET_USERS",
-      payload: items
-    });
-  };
-
-  //get initial users(testing purposes)
-  // const fetchUsers = async () => {
-  //   setLoading();
-
-  //   const res = await fetch(`${GITHUB_URL}/users`, {
-  //     headers: {
-  //       Authorization: `token ${GITHUB_TOKEN}`,
-  //     },
-  //   });
-
-  //   const data = await res.json();
-
-  //   dispatch({
-  //     type: "GET_USERS",
-  //     payload: data
-  //   });
-  // };
-  
-  //set loading
-  const setLoading = () => dispatch({ type: "SET_LOADING" });
-
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        searchUsers,
+        ...state,
+        dispatch,
       }}
     >
       {children}
--- src/context/github/GithubReducer.jsx
+++ src/context/github/GithubReducer.jsx
@@ -1,21 +1,40 @@
-import loading from 'daisyui/components/loading'
+import loading from "daisyui/components/loading";
 
-const githubReducer = (state, action) =>{
-    switch (action.type){
-        case 'GET_USERS':
-            return {
-                ...state,
-                users:action.payload,
-                loading:false
-            }
-        case 'SET_LOADING':
-            return {
-                ...state,
-                loading: true
-            }
-        default:
-            return state
-    }
-}
+const githubReducer = (state, action) => {
+  switch (action.type) {
+    case "GET_USERS":
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    case "GET_USER_AND_REPOS":
+      return {
+        ...state,
+        user: action.payload.user,
+        repos: action.payload.repos,
 
-export default githubReducer
+        loading: false,
+      };
+    // case "GET_REPOS":
+    //   return {
+    //     ...state,
+    //     repos: action.payload,
+    //     loading: false,
+    //   };
+    case "SET_LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "CLEAR_USERS":
+      return {
+        ...state,
+        users: [],
+      };
+    default:
+      return state;
+  }
+};
+
+export default githubReducer;
