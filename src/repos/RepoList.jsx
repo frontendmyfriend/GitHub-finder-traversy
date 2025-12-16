95988aa080210805107bc58c15c127d990b2bbf5
frontendmyfriend <45656043+frontendmyfriend@users.noreply.github.com>
2025-12-16 16:41:57 +0200

    Update RepoList.jsx

--- src/repos/RepoList.jsx
+++ src/repos/RepoList.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types'
+
 import RepoItem from './RepoItem'
 
 function RepoList({ repos }) {
@@ -16,8 +16,5 @@ function RepoList({ repos }) {
   )
 }
 
-RepoList.propTypes = {
-  repos: PropTypes.array.isRequired,
-}
 
 export default RepoList
